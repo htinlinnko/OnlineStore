531786d501d3b26f2c165abaab3b2fe8577272e7
htinlinnko <htinlinnko.sg@gmail.com>
2016-05-05 16:13:09 +0800

    Service file

---
+++ POSService/App_Code/TestService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for TestService
+/// </summary>
+public class TestService : ITest
+{
+    public TestService()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public string getHello()
+    {
+        return "Hello World";
+    }
+
+    public string anotherHello()
+    {
+        return "Another one";
+    }
+}
